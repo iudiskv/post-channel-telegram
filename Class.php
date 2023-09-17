90af3b8ab7a5d719ed9a8fc48731b56e4213b355
Soloma <125241138+iudiskv@users.noreply.github.com>
2023-09-17 09:43:01 +0200

    Add files via upload

---
+++ Class.php
@@ -0,0 +1,210 @@
+﻿<?php 
+/* ob Start */
+ob_start();
+/* Token Bot */
+define('API_KEY','');
+/* Admin List */
+$admin =  "";
+$admin1 = "";
+$admin2 = "";
+/* Tabee Save */
+function save($filename,$TXTdata){
+	$myfile = fopen($filename, "w") or die("Unable to open file!");
+	fwrite($myfile, "$TXTdata");
+	fclose($myfile);
+	}
+/* Tabee Bot OFficial */
+function bot($method,$datas=[]){
+    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
+}
+/* Tabee Bot XYZ */
+function botXYZ($method,$datas=[]){
+    $url = "https://api.pwrtelegram.xyz/bot".API_KEY."/".$method;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
+}
+/* Tabee Send Message */
+function SendMessage($chatid,$text,$parsmde,$disable_web_page_preview,$keyboard){
+	bot('sendMessage',[
+	'chat_id'=>$chatid,
+	'text'=>$text,
+	'parse_mode'=>$parsmde,
+	'disable_web_page_preview'=>$disable_web_page_preview,
+	'reply_markup'=>$keyboard
+	]);
+	}
+/* Tabee Send Message */
+function SendMessageadmin($chatid,$text,$parsmde,$disable_web_page_preview,$keyboard){
+	bot('sendMessage',[
+	'chat_id'=>$chatid,
+	'text'=>$text,
+	'parse_mode'=>$parsmde,
+	'disable_web_page_preview'=>$disable_web_page_preview,
+	'reply_markup'=>$keyboard
+	]);
+	}
+	/* Tabee Forward Message */
+function ForwardMessage($chatid,$from_chat,$message_id){
+	bot('ForwardMessage',[
+	'chat_id'=>$chatid,
+	'from_chat_id'=>$from_chat,
+	'message_id'=>$message_id
+	]);
+	}
+	/* Tabee Send Photo */
+function SendPhoto($chatid,$photo,$keyboard,$caption){
+	bot('SendPhoto',[
+	'chat_id'=>$chatid,
+	'photo'=>$photo,
+	'caption'=>$caption,
+	'reply_markup'=>$keyboard
+	]);
+	}
+	/* Tabee Send Audio */
+function SendAudio($chatid,$audio,$keyboard,$caption,$sazande,$title){
+	bot('SendAudio',[
+	'chat_id'=>$chatid,
+	'audio'=>$audio,
+	'caption'=>$caption,
+	'performer'=>$sazande,
+	'title'=>$title,
+	'reply_markup'=>$keyboard
+	]);
+	}
+	/* Tabee Send Document */
+function SendDocument($chatid,$document,$keyboard,$caption){
+	bot('SendDocument',[
+	'chat_id'=>$chatid,
+	'document'=>$document,
+	'caption'=>$caption,
+	'reply_markup'=>$keyboard
+	]);
+	}
+	/* Tabee Send Sticker */
+function SendSticker($chatid,$sticker,$keyboard){
+	bot('SendSticker',[
+	'chat_id'=>$chatid,
+	'sticker'=>$sticker,
+	'reply_markup'=>json_encode([
+            'inline_keyboard'=>[
+       [
+                          ['text'=>"🐦 ارسال توئیت و پیام شما",'url'=>'https://telegram.me/apnu_tweetbot']
+
+]]])
+	]);
+	}
+	/* Tabee Send Video */
+function SendVideo($chatid,$video,$caption,$keyboard,$duration){
+	bot('SendVideo',[
+	'chat_id'=>$chatid,
+	'video'=>$video,
+        'caption'=>$caption,
+	'duration'=>$duration,
+	'reply_markup'=>$keyboard
+	]);
+	}
+	/* Tabee Send Voice */
+function SendVoice($chatid,$voice,$keyboard,$caption){
+	bot('SendVoice',[
+	'chat_id'=>$chatid,
+	'voice'=>$voice,
+	'caption'=>$caption,
+	'reply_markup'=>$keyboard
+	]);
+	}
+	/* Tabee Send Contact */
+function SendContact($chatid,$first_name,$phone_number,$keyboard){
+	bot('SendContact',[
+	'chat_id'=>$chatid,
+	'first_name'=>$first_name,
+	'phone_number'=>$phone_number,
+	'reply_markup'=>$keyboard
+	]);
+	}
+	/* Tabee Send Chat Action */
+function SendChatAction($chatid,$action){
+	bot('sendChatAction',[
+	'chat_id'=>$chatid,
+	'action'=>$action
+	]);
+	}
+	/* Tabee Kick Chat Member */
+function KickChatMember($chatid,$user_id){
+	bot('kickChatMember',[
+	'chat_id'=>$chatid,
+	'user_id'=>$user_id
+	]);
+	}
+	/* Tabee Leave Chat */
+function LeaveChat($chatid){
+	bot('LeaveChat',[
+	'chat_id'=>$chatid
+	]);
+	}
+	/* Tabee Get Chat */
+function getChat($idchat){
+	$json=file_get_contents('https://api.telegram.org/bot'.API_KEY."/getChat?chat_id=".$idchat);
+	$data=json_decode($json,true);
+	return $data["result"]["first_name"];
+}
+	/* Tabee Get Chat Members Count */
+function GetChatMembersCount($chatid){
+	bot('getChatMembersCount',[
+	'chat_id'=>$chatid
+	]);
+	}
+	/* Tabee Get Chat Member */
+function GetChatMember($chatid,$userid){
+	$truechannel = json_decode(file_get_contents('https://api.telegram.org/bot'.API_KEY."/getChatMember?chat_id=".$chatid."&user_id=".$userid));
+	$tch = $truechannel->result->status;
+	return $tch;
+	}
+	/* Tabee Answer Callback Query */
+function AnswerCallbackQuery($callback_query_id,$text,$show_alert){
+	bot('answerCallbackQuery',[
+        'callback_query_id'=>$callback_query_id,
+        'text'=>$text,
+		'show_alert'=>$show_alert
+    ]);
+	}
+	/* Tabee Edit Message Text */
+function EditMessageText($chat_id,$message_id,$text,$parse_mode,$disable_web_page_preview,$keyboard){
+	 bot('editMessagetext',[
+    'chat_id'=>$chat_id,
+	'message_id'=>$message_id,
+    'text'=>$text,
+    'parse_mode'=>$parse_mode,
+	'disable_web_page_preview'=>$disable_web_page_preview,
+    'reply_markup'=>$keyboard
+	]);
+	}
+	/* Tabee Edit Message Caption */
+function EditMessageCaption($chat_id,$message_id,$caption,$keyboard,$inline_message_id){
+	 bot('editMessageCaption',[
+    'chat_id'=>$chat_id,
+	'message_id'=>$message_id,
+    'caption'=>$caption,
+    'reply_markup'=>$keyboard,
+	'inline_message_id'=>$inline_message_id
+	]);
+	}
+	
+	?>
---
+++ index.php
@@ -0,0 +1,105 @@
+<?php
+
+include 'Class.php';
+
+// Variable Source
+$update = json_decode(file_get_contents('php://input'));
+$from_id = $update->message->from->id;
+$chat_id = $update->message->chat->id;
+$message_id = $update->message->message_id;
+$forward = $update->message->forward_from->id;
+$first = $update->message->from->first_name;
+$username = $update->message->from->username;
+$inline = $update->inline_query;
+$inline_text = $update->inline_query->query;
+$first_inline = $update->inline_query->from->first_name;
+$command = file_get_contents("user/".$from_id."/command.txt");
+$code = file_get_contents("user/".$from_id."/code.txt");
+$channel = "@Nic_Source";
+$group_id = '-1001149800912';
+$botname = "https://t.me/Nic_Source";
+$ptext  = "سورس رایگان ربات تلگرام";
+$sticker = $update->message->sticker->file_id;
+$video = $update->message->video->file_id;
+$voice = $update->message->voice->file_id;
+$file = $update->message->document->file_id;
+$photo2 = $update->message->photo[2]->file_id;
+$photo1 = $update->message->photo[1]->file_id;
+$photo0 = $update->message->photo[0]->file_id;
+$music = $update->message->audio->file_id;
+$text = $update->message->text;
+$caption = $update->message->caption;
+$url_pattern = '/(([\w]+:)?\/\/)?(([\d\w]|%[a-fA-f\d]{2,2})+(:([\d\w]|%[a-fA-f\d]{2,2})+)?@)?([\d\w]([-\d\w]{0,253}[\d\w])?\.)+[\w]{2,4}(:[\d]+)?(\/([-+_~.,\d\w]|%[a-fA-f\d]{2,2})*)*(\?(&?([-+_~.,\d\w]|%[a-fA-f\d]{2,2})=?)*)?(#([-+_~.,\/\d\w]|%[a-fA-f\d]{2,2})*)?$/'; 	
+	//======================== تمیز کردن از ای دی
+	if ( preg_match("/@[\w\-]+/", $text))
+{$pattern = '/@[\w\-]+/';
+$replacement = '';
+$text= preg_replace($pattern, $replacement, $text);}
+if ( preg_match("/@[\w\-]+/", $caption))
+{$pattern = '/@[\w\-]+/';
+$replacement = '';
+$caption= preg_replace($pattern, $replacement, $caption);}
+//======================== تمیز کردن از لینک
+if (preg_match($url_pattern, $caption)) {
+$replacement='';    
+$caption = preg_replace($url_pattern, $replacement, $caption);}
+if (preg_match($url_pattern, $text)) {
+$replacement='';    
+$text = preg_replace($url_pattern, $replacement, $text);}
+//======================== تمیز کردن از لینک
+	
+	//========================
+	if(preg_match('/^\/([Ss]tart)/',$text) and $from_id == $admin || $from_id == $admin1 || $from_id == $admin2){
+	SendChatAction($chat_id,"typing");
+	SendMessage($chat_id,"سلام ادمین عزیز خوش اومدی از این به بعد میتونی پیام هارو برای من فوروارد کنی تا من بدون منبع توی کانال قرار بدم:(","html","true");
+	}
+	elseif($sticker != null  and $from_id == $admin || $from_id == $admin1 || $from_id == $admin2){
+	SendChatAction($chat_id,"typing");
+	SendMessageadmin($chat_id,"ارسال شد.","html","true");
+	SendSticker($channel,$sticker);
+
+	}
+	elseif($video != null  and $from_id == $admin || $from_id == $admin1 || $from_id == $admin2){
+	SendChatAction($chat_id,"typing");
+	SendMessageadmin($chat_id,"ارسال شد.","html","true");
+	SendVideo($channel,$video,"$caption\n$channel\n$botname");
+	}
+	elseif($voice != null  and $from_id == $admin || $from_id == $admin1 || $from_id == $admin2){
+	SendChatAction($chat_id,"typing");
+	SendMessageadmin($chat_id,"ارسال شد.","html","true");
+	SendVoice($channel,$voice,'',"$caption\n$channel\n$botname");
+	}
+	elseif($file != null  and $from_id == $admin || $from_id == $admin1 || $from_id == $admin2){
+	SendChatAction($chat_id,"typing");
+	SendMessageadmin($chat_id,"ارسال شد.","html","true");
+	SendDocument($channel,$file,'',"$caption\n$channel\n$botname");
+	}
+	elseif($music != null  and $from_id == $admin || $from_id == $admin1 || $from_id == $admin2){
+	SendChatAction($chat_id,"typing");
+	SendMessageadmin($chat_id,"ارسال شد.","html","true");
+	SendAudio($channel,$music,'',"$caption\n$channel\n$botname");
+	}
+	elseif($text != null  and $from_id == $admin || $from_id == $admin1 || $from_id == $admin2){
+	SendChatAction($chat_id,"typing");
+	SendMessageadmin($chat_id,"ارسال شد.","html","true");
+	SendMessage($channel,"$text\n$ptext\n$channel\n $botname ","html","true");
+  SendMessage($group_id,"$text\n$ptext\n$channel\n $botname\n-------------------------\n این پیام توسط $username عزیز  به کانال ارسال گردید","html","true");
+}
+	elseif($photo2 != null and $from_id == $admin || $from_id == $admin1 || $from_id == $admin2){
+	SendChatAction($chat_id,"typing");
+	SendMessageadmin($chat_id,"ارسال شد.","html","true");
+	SendPhoto($channel,$photo2,'',"$caption\n$channel\n$botname");
+	}
+	elseif($photo1 != null and $from_id == $admin || $from_id == $admin1 || $from_id == $admin2){
+	SendChatAction($chat_id,"typing");
+	SendMessageadmin($chat_id,"ارسال شد.","html","true");
+	SendPhoto($channel,$photo1,'',"$caption\n$channel\n$botname");
+	}
+	elseif($photo0 != null and $from_id == $admin || $from_id == $admin1 || $from_id == $admin2){
+	SendChatAction($chat_id,"typing");
+	SendMessageadmin($chat_id,"ارسال شد.","html","true");
+	SendPhoto($channel,$photo0,'',"$caption\n$channel\n$botname");
+	}
+	
+
+	?>
